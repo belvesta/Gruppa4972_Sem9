6050336ffabf6de30ba5d75d941402fbc90da748
belvesta <143174841+belvesta@users.noreply.github.com>
2023-10-12 00:42:23 +0300

    Add

---
+++ Sem9Task64/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные
+// числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите значение N: ");
+        int n = int.Parse(Console.ReadLine());
+
+        if (n < 1)
+        {
+            Console.WriteLine("N должно быть натуральным числом.");
+        }
+        else
+        {
+            PrintNaturalNumbers(n);
+        }
+    }
+
+    static void PrintNaturalNumbers(int n)
+    {
+        if (n == 0)
+        {
+            return;
+        }
+        else
+        {
+            Console.WriteLine(n);
+            PrintNaturalNumbers(n - 1);
+        }
+    }
+}
---
+++ Sem9Task66/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму
+// натуральных элементов в промежутке от M до N.
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите значение M: ");
+        int m = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите значение N: ");
+        int n = int.Parse(Console.ReadLine());
+
+        if (m <= n)
+        {
+            int sum = SumNaturalNumbersInRange(m, n);
+            Console.WriteLine($"Сумма натуральных чисел в промежутке от {m} до {n}: {sum}");
+        }
+        else
+        {
+            Console.WriteLine("M должно быть меньше или равно N.");
+        }
+    }
+
+    static int SumNaturalNumbersInRange(int m, int n)
+    {
+        int sum = 0;
+
+        for (int i = m; i <= n; i++)
+        {
+            if (i > 0)
+            {
+                sum += i;
+            }
+        }
+
+        return sum;
+    }
+}
